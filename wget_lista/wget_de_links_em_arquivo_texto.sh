a9a3cc580b44791bad2f4c8192bab370518a59cc
fernandoreycaires <fernando.rey.caires@gmail.com>
2023-12-20 16:55:57 -0300

    Novos Scripts

---
+++ wget_lista/wget_de_links_em_arquivo_texto.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Verifica se o número correto de argumentos foi fornecido
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <arquivo_de_entrada>"
+    exit 1
+fi
+
+# Armazena o nome do arquivo de entrada
+arquivo_entrada=$1
+
+# Verifica se o arquivo de entrada existe
+if [ ! -f "$arquivo_entrada" ]; then
+    echo "O arquivo de entrada $arquivo_entrada não existe."
+    exit 1
+fi
+
+
+# Varre arquivo e executa o wget em cada linha
+
+while IFS= read -r linha; do
+    wget "${linha}"
+done < "$arquivo_entrada"
+
+echo "Operação concluída. Resultado salvo em $arquivo_saida."
+
+# De permição de execução no script
+# chmod +x editar_linhas_arquivo_texto.sh
+# Para executar
+# ./editar_linhas_arquivo_texto.sh arquivo_de_texto.txt
+
